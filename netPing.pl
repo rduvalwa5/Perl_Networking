5904565049d20dd7889f8fae68584f33490a1b36
rduvalwa2 <rduvalwa2@maxbookpro17osx.hsd1.wa.comcast.net>
2021-02-26 17:16:03 -0800

    add netping.pl

--- netPing.pl
+++ netPing.pl
@@ -1,53 +1,23 @@
+=begin comment
+https://perldoc.perl.org/Net::Ping
+=cut
+
+
+#!/usr/bin/perl
+
+=begin comment use strict; 
+=cut
+use warnings;
+use 5.010;
 use Net::Ping;
 
-$p = Net::Ping->new();
-print "$host is alive.\n" if $p->ping($host);
-$p->close();
-
-$p = Net::Ping->new("icmp");
-$p->bind($my_addr); # Specify source interface of pings
-foreach $host (@host_array)
-{
-    print "$host is ";
-    print "NOT " unless $p->ping($host, 2);
-    print "reachable.\n";
-    sleep(1);
+    
+@nethosts =("localhost","osxair","MaxBookPro17OSX","C1246895-XPS","powerbookg4"."Jacquelyns-iMac","73.118.226.124","www.comcast");
+    
+for (@nethosts){
+    my $p = Net::Ping->new();
+    if ($p->ping("$_")) {
+       print  "$_ alive\n";
+    }
+    else{ print  "$_ is not alive\n";}
 }
-$p->close();
-
-=Begin comment
-
-$p = Net::Ping->new("tcp", 2);
-# Try connecting to the www port instead of the echo port
-$p->port_number(scalar(getservbyname("http", "tcp")));
-while ($stop_time > time())
-{
-    print "$host not reachable ", scalar(localtime()), "\n"
-        unless $p->ping($host);
-    sleep(300);
-}
-undef($p);
-
-# Like tcp protocol, but with many hosts
-$p = Net::Ping->new("syn");
-$p->port_number(getservbyname("http", "tcp"));
-foreach $host (@host_array) {
-  $p->ping($host);
-}
-while (($host,$rtt,$ip) = $p->ack) {
-  print "HOST: $host [$ip] ACKed in $rtt seconds.\n";
-}
-
-# High precision syntax (requires Time::HiRes)
-$p = Net::Ping->new();
-$p->hires();
-($ret, $duration, $ip) = $p->ping($host, 5.5);
-printf("$host [ip: $ip] is alive (packet return time: %.2f ms)\n",
-        1000 * $duration)
-  if $ret;
-$p->close();
-
-# For backward compatibility
-print "$host is alive.\n" if pingecho($host);
-
-=cut
